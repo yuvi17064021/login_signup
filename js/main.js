0537b50453d21afa758ffc668228deb9acd452cc
Yuvraj Singh <39516634+yuvi17064021@users.noreply.github.com>
2018-08-22 19:37:10 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,10 +13,10 @@
             else {
                 $(this).removeClass('has-val');
             }
-        })    
+        })
     })
-  
-  
+
+
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
@@ -65,7 +65,80 @@
 
         $(thisAlert).removeClass('alert-validate');
     }
-    
-    
+
+
+
+})(jQuery);
+
+
+
+
+
+(function ($) {
+    "use strict";
+
+
+     /*==================================================================
+    [ Focus input ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })
+    })
+
+
+var input = $('.validate1-input .input100');
+
+$('.validate1-form').on('submit',function(){
+    var check = true;
+
+    for(var i=0; i<input.length; i++) {
+        if(validate1(input[i]) == false){
+            showValidate1(input[i]);
+            check=false;
+        }
+    }
+
+    return check;
+});
+
+
+$('.validate1-form .input100').each(function(){
+    $(this).focus(function(){
+       hideValidate1(this);
+    });
+});
+
+function validate1 (input) {
+    if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+        if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            return false;
+        }
+    }
+    else {
+        if($(input).val().trim() == ''){
+            return false;
+        }
+    }
+}
+
+function showValidate1(input) {
+    var thisAlert = $(input).parent();
+
+    $(thisAlert).addClass('alert-validate1');
+}
+
+function hideValidate1(input) {
+    var thisAlert = $(input).parent();
+
+    $(thisAlert).removeClass('alert-validate1');
+}
+
+
 
 })(jQuery);
